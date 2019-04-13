9f8d85a9dd2c049322b35fefbd072c1eeda57dc1
aipsupraptomunari <aipsupraptomunari@gmail.com>
2019-04-14 03:29:13 +0700

    chapter 7 teori dan praktek

---
+++ src/1164063/chapter7-10.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 19:59:26 2019
+
+@author: kiting13
+"""
+
+model.compile(loss='categorical_crossentropy', optimizer='adamax', metrics=['accuracy'])
